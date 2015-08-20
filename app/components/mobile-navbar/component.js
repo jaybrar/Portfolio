b5b353f5364fb9065c07458d41f4e66243991b29
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-08-20 12:02:26 -0700

    change

--- app/components/mobile-navbar/component.js
+++ app/components/mobile-navbar/component.js
@@ -4,8 +4,10 @@ export default Ember.Component.extend({
 	didInsertElement: function() {
 		$('.button-collapse').sideNav();
 		$('ul li').click(function(){
+			if($(this).text() != 'LinkedIn' && $(this).text() != "GitHub"){
 			$('li').removeClass("active red darken-1");
 			$(this).addClass("active red darken-1")
+			}
 		})
 	}
 });
