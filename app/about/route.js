965fe8338a2eea26ea60fe913ceb7ef5baada716
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-08-20 15:00:24 -0700

    changes

--- app/about/route.js
+++ app/about/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	setupController: function(controller, model){
-		controller.set('model', ['HTML', 'CSS3', 'JavaScript','Ajax','Git','PHP','SQL','Ruby','CodeIgnighter','Express.js','Angular.js','Ember.js','Rails','Node.js','Github','Sinatra','Soket.io','MongoDb','PostgreSql','Sqlite3','jQuery','Bootstrap','Foundation','Materialize']);
+		controller.set('model', ['HTML', 'CSS3', 'JavaScript','Ajax','Git','PHP','SQL','Ruby','CodeIgnighter','Express.js','Angular.js','Ember.js','Rails','Node.js','Github','Sinatra','Socket.io','MongoDB','PostgreSQL','Sqlite3','jQuery','Bootstrap','Foundation','Materialize']);
 	}
 });
