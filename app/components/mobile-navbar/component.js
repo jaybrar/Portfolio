d964c7b29bc365477e9bb256f7f207e9d9d70ada
Jay Brar <jaybrar79@gmail.com>
2016-07-19 13:33:36 -0700

    changes

--- app/components/mobile-navbar/component.js
+++ app/components/mobile-navbar/component.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	didInsertElement: function() {
 		$('.button-collapse').sideNav();
-		$('.nav-wrapper ul li').click(function(){
-			if($(this).text() != 'LinkedIn' && $(this).text() != "GitHub"){
-			$('li').removeClass("active red darken-1");
-			$(this).addClass("active red darken-1");
-			}
-		})
+		// $('.nav-wrapper ul li').click(function(){
+		// 	if($(this).text() != 'LinkedIn' && $(this).text() != "GitHub"){
+		// 	$('li').removeClass("active red darken-1");
+		// 	$(this).addClass("active red darken-1");
+		// 	}
+		// })
 	}
 });
